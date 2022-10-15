12cadd78ac0b8777ceea625f278c1ba9a953e357
Madison Abel <madison.abel@qu.edu>
2022-10-14 20:02:42 -0400

    area_script1.py was edited to include a fuction that takes an argument and calculates area

--- area_script1.py
+++ area_script1.py
@@ -1,6 +1,6 @@
 This Script includes a Function called myarea() that takes User Input and Calculates Area.
 
-def myarea():
+def myarea(l,w):
 	#This function asks the user to enter a value for length and a value for width. This function calculates the area of the values entered by the user.
 	l = int(input(' Enter a value for length: '))
 	w = int(input(' Enter a value for width: '))
